17c9a833ca629a72a44c71ddab2b3e0e7cfd182e
AbelVaradi <AbelVaradi@ABELVARADI-PC>
2017-06-09 21:30:53 +0200

    Separated Person class from Main

---
+++ CreateClass/CreateClass/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    public enum Genders { male, female }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Genders gender;
+
+        public Person(string name, DateTime birthdate, Genders gender)
+        {
+            this.Name = name;
+            this.BirthDate = birthdate;
+            this.gender = gender;
+        }
+
+
+        public override string ToString()
+        {
+            return this.Name + " " + this.BirthDate + " " + this.gender;
+        }
+    }
+}
--- CreateClass/CreateClass/Program.cs
+++ CreateClass/CreateClass/Program.cs
@@ -4,29 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+     
 namespace CreateClass
 {
-    class Person
+    class Program
     {
-        public enum Genders { male, female }
-
-        public string Name { get; set; }
-        public DateTime BirthDate { get; set; }
-        public Genders gender;
-        
-        public Person(string name, DateTime birthdate, Genders gender )
-        {
-            this.Name = name;
-            this.BirthDate = birthdate;
-            this.gender = gender;
-        }
-
-
-        public override string ToString()
-        {
-            return this.Name + " " + this.BirthDate + " " + this.gender;
-        }
-
         static void Main(string[] args)
         {
             Person abel = new Person("Ábel Váradi", new DateTime(1984,03,28), Genders.male);
